678c1679ce2be98e96f76e3c1db3d954e4e221c6
SpiderVice <spidervice@gmail.com>
2025-12-17 18:16:01 +0000

    tweak(misc): bump personal build number up because cache

--- changedetectionio/__init__.py
+++ changedetectionio/__init__.py
@@ -2,7 +2,7 @@
 
 # Read more https://github.com/dgtlmoon/changedetection.io/wiki
 
-__version__ = '0.51.4-svdev'
+__version__ = '0.51.4-svdev-2'
 
 from changedetectionio.strtobool import strtobool
 from json.decoder import JSONDecodeError
